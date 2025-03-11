311b3cadd0f042dbb9c46a9d1a1418ae1c0b1027
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-03-11 23:03:00 +0200

    All skeletons done

---
+++ momento-server/models/PhotoTagSkeleton.php
@@ -0,0 +1,12 @@
+<?php 
+class PhotoTagSkeleto {
+
+    public static $photo_id;
+    public static $tag_id;
+
+    public static function create($photo_id, $tag_id){
+        self::$photo_id = $photo_id;
+        self::$tag_id = $tag_id;
+    }
+
+}
