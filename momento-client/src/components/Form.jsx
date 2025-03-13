5999d5cd59c0223ed7f04864341e77d7cfd92719
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-03-13 03:37:59 +0200

    login page implemented

--- momento-client/src/components/Form.jsx
+++ momento-client/src/components/Form.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import "../css/components/forms.css"; 
+import Button from './button';
 
 const Form = () => {
+    const handleClick = () => {
+        console.log("Button clicked!");
+      };
+
   return (
-    <div></div>
+    <div className='form-container flex column'>
+        <h1>Welcome Home!</h1>
+        <h3>Your memories await</h3>
+        <div className="form-group">
+            <label  htmlFor="">Email</label>
+            <input className='input-field' type="text" placeholder='example@email.com'/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="">Password</label>
+            <input className='input-field' type="password" placeholder='abc123'/>
+        </div>
+        <Button text= "login" onClick={handleClick}/>
+        <p>First time here? <a href="">Create your memory box</a></p>
+    </div>
   );
 };
 
--- momento-client/src/pages/Login.jsx
+++ momento-client/src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../css/pages/login.css"; 
+import Form from '../components/form';
+
 
 const Login = () => {
   return (
-    <div>
-      Login Page
-    </div>
+    <Form/>
   );
 };
 
