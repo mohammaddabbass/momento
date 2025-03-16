7811593a52874d7c4972e29bfbaa8d8c9eb74961
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-03-17 00:57:10 +0200

    fixed get photos response naming

--- momento-server/api/v1/photoController.php
+++ momento-server/api/v1/photoController.php
@@ -1,8 +1,14 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: *');
-header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
-// header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+// Handle preflight OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require(__DIR__ . '../../../models/Photo.php');
 
@@ -25,7 +31,7 @@ class PhotoController {
             exit;
         }
 
-        echo json_encode(["message" => 'photos fetched successfully', "data" => $photos]);
+        echo json_encode(["message" => 'photos fetched successfully', "photos" => $photos]);
     }
 
     static function uploadPhoto() {
